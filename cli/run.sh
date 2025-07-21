292b48b6006474fb9bbbfc9d0f12663cb4fd661c
Hansimov <591172499@qq.com>
2025-07-21 19:57:19 +0800

    :zap: [Enhance] run.sh: add new -e arg to extract only

--- cli/run.sh
+++ cli/run.sh
@@ -1,8 +1,17 @@
-# zepto must be run first, as it uses proxy, and would close browser when finished
-python -m web.zepto.batcher -s -e
-python -m web.blinkit.batcher -s -e
-# swiggy must be run last, as it uses data of blinkit and zepto
-python -m web.swiggy.batcher -s -e
-python -m file.excel_merger
+if [ "$1" = "-e" ]; then
+    # extract only
+    python -m web.zepto.batcher -e
+    python -m web.blinkit.batcher -e
+    python -m web.swiggy.batcher -e
+    python -m file.excel_merger
+else
+    # zepto must be run first, as it uses proxy, and would close browser when finished
+    python -m web.zepto.batcher -s -e
+    python -m web.blinkit.batcher -s -e
+    # swiggy must be run last, as it uses data of blinkit and zepto
+    python -m web.swiggy.batcher -s -e
+    python -m file.excel_merger
+fi
 
+# ./cli/run.sh -e
 # ./cli/run.sh
