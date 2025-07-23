1f50c73daab8238cca1a176665572f1afbfff44d
Hansimov <591172499@qq.com>
2025-07-23 19:44:45 +0800

    :zap: [Enhance] ImageMatcher: simplify codes; LocationClicker: add params of tab and suffix, replace ImageGrab to tab for screenshot, and replace pyautogui to tab.run_cdp for click and type, which fits multiple browsers and windows

--- web/clicker.py
+++ web/clicker.py
@@ -1,9 +1,8 @@
 import cv2
-import pyautogui
 
-from mss import mss
+from DrissionPage._pages.chromium_tab import ChromiumTab
 from pathlib import Path
-from PIL import Image, ImageGrab
+from time import sleep
 
 from configs.envs import IMGS_ROOT
 
@@ -12,7 +11,9 @@ class ImageMatcher:
     def __init__(self, source_image_path: Path, template_image_path: Path):
         self.source_image = cv2.imread(str(source_image_path))
         self.template_image = cv2.imread(str(template_image_path))
-        self.detected_image_path = source_image_path.parent / "screenshot_detected.png"
+        self.detected_image_path = (
+            source_image_path.parent / f"{source_image_path.stem}_detected.png"
+        )
 
     def match(self):
         """
@@ -24,89 +25,99 @@ class ImageMatcher:
         res = cv2.matchTemplate(
             self.source_image, self.template_image, cv2.TM_CCOEFF_NORMED
         )
-        _, _, _, match_location = cv2.minMaxLoc(res)
-        match_left = match_location[0]
-        match_top = match_location[1]
-        match_right = match_location[0] + self.template_image.shape[1]
-        match_bottom = match_location[1] + self.template_image.shape[0]
-        match_region = (match_left, match_top, match_right, match_bottom)
-
-        self.match_region = match_region
-        return match_region
+        _, _, _, (left, top) = cv2.minMaxLoc(res)
+        right = left + self.template_image.shape[1]
+        bottom = top + self.template_image.shape[0]
+        self.match_region = (left, top, right, bottom)
+        return self.match_region
 
     def draw_rectangle(self):
         cv2.rectangle(
             img=self.source_image,
             pt1=self.match_region[:2],
             pt2=self.match_region[2:],
-            color=(0, 255, 0),
+            color=(0, 255, 0),  # BGR
             thickness=2,
-        )  # BGR
+        )
         cv2.imwrite(str(self.detected_image_path), self.source_image)
-
-    def display(self):
-        cv2.imshow("Detected", self.source_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
 
 class LocationClicker:
-    def __init__(self, location_image_name: str = None):
-        self.screenshot_image_path = IMGS_ROOT / "screenshot.png"
-        if location_image_name is not None:
-            self.set_location_image_name(location_image_name)
+    def __init__(self, tab: ChromiumTab = None, suffix: str = ""):
+        self.tab = tab
+        self.suffix = suffix
+        self.init_paths()
+
+    def init_paths(self):
+        screenshot_name = "screenshot"
+        if self.suffix:
+            screenshot_name = f"{screenshot_name}_{self.suffix}"
+        self.screenshot_image_path = IMGS_ROOT / f"{screenshot_name}.png"
 
     def set_location_image_name(self, location_image_name: str):
         self.location_image_name = location_image_name
         self.location_image_path = IMGS_ROOT / self.location_image_name
 
     def get_screenshot(self):
-        ImageGrab.grab(all_screens=True).save(self.screenshot_image_path)
+        self.tab.get_screenshot(path=self.screenshot_image_path, full_page=False)
 
     def get_location_item_position(self):
-        with mss() as sct:
-            all_monitor = sct.monitors[0]
-            monitor_left_offset = all_monitor["left"]
-            monitor_top_offset = all_monitor["top"]
-
-        image_matcher = ImageMatcher(
+        self.get_screenshot()
+        matcher = ImageMatcher(
             source_image_path=self.screenshot_image_path,
             template_image_path=self.location_image_path,
         )
-
-        match_region = image_matcher.match()
-        image_matcher.draw_rectangle()
-        # image_matcher.display()
-        match_region_in_monitor = (
-            match_region[0] + monitor_left_offset,
-            match_region[1] + monitor_top_offset,
-            match_region[2] + monitor_left_offset,
-            match_region[3] + monitor_top_offset,
-        )
-        region_center = (
-            (match_region_in_monitor[0] + match_region_in_monitor[2]) / 2,
-            (match_region_in_monitor[1] + match_region_in_monitor[3]) / 2,
-        )
-        return region_center
+        left, top, right, bottom = matcher.match()
+        matcher.draw_rectangle()
+        center_x = (left + right) / 2
+        center_y = (top + bottom) / 2
+        return center_x, center_y
 
     def click_target_position(self):
-        self.get_screenshot()
-        location_item_center = self.get_location_item_position()
-        pyautogui.moveTo(*location_item_center)
-        pyautogui.click()
+        x, y = self.get_location_item_position()
+        for event in ("mousePressed", "mouseReleased"):
+            self.tab.run_cdp(
+                "Input.dispatchMouseEvent",
+                type=event,
+                x=x,
+                y=y,
+                button="left",
+                clickCount=1,
+            )
+
+
+class BlinkitLocationClicker(LocationClicker):
+    def __init__(self, tab: ChromiumTab, suffix: str = "blinkit"):
+        super().__init__(tab=tab, suffix=suffix)
 
 
 class SwiggyLocationClicker(LocationClicker):
+    def __init__(self, tab: ChromiumTab, suffix: str = "swiggy"):
+        super().__init__(tab=tab, suffix=suffix)
+
     def type_target_location_text(self, location_text: str):
-        self.get_screenshot()
-        location_item_center = self.get_location_item_position()
-        pyautogui.moveTo(*location_item_center)
-        pyautogui.click(clicks=3)
-        pyautogui.press("backspace")
-        pyautogui.typewrite(location_text)
+        # select input field with triple clicks
+        x, y = self.get_location_item_position()
+        for event in ("mousePressed", "mouseReleased"):
+            self.tab.run_cdp(
+                "Input.dispatchMouseEvent",
+                type=event,
+                x=x,
+                y=y,
+                button="left",
+                clickCount=3,
+            )
 
+        # delete existing text
+        BACKSPACE_KEY_CODE = 8
+        for event in ("keyDown", "keyUp"):
+            self.tab.run_cdp(
+                "Input.dispatchKeyEvent",
+                type=event,
+                windowsVirtualKeyCode=BACKSPACE_KEY_CODE,
+            )
 
-if __name__ == "__main__":
-    clicker = LocationClicker()
-
-    # python -m web.clicker
+        # type location text
+        for ch in location_text:
+            self.tab.run_cdp("Input.dispatchKeyEvent", type="char", text=ch)
+            sleep(0.05)
