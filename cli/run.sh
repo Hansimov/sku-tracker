9b6d87c55ae36126d0499ddbd9ea428db08d98e4
Hansimov <591172499@qq.com>
2025-07-24 13:15:08 +0800

    :zap: [Enhance] run.sh: rewrite arg parses to support more flexible options

--- cli/run.sh
+++ cli/run.sh
@@ -1,17 +1,40 @@
-if [ "$1" = "-e" ]; then
-    # extract only
+is_scrape=false
+is_extract=false
+is_postprocess=false
+
+for arg in "$@"; do
+    if [ "$arg" = "-s" ]; then
+        is_scrape=true
+    elif [ "$arg" = "-e" ]; then
+        is_extract=true
+    elif [ "$arg" = "-x" ]; then
+        is_postprocess=true
+    fi
+done
+
+if [ "$is_scrape" = true ]; then
+    # zepto must run first, as it uses proxy, and would close browser when finished
+    python -m web.zepto.scraper -s -c
+    python -m web.blinkit.scraper -s
+    # swiggy must run last, as it uses data of blinkit and zepto
+    python -m web.swiggy.scraper -s -c
+fi
+
+if [ "$is_extract" = true ]; then
     python -m web.zepto.batcher -e
     python -m web.blinkit.batcher -e
     python -m web.swiggy.batcher -e
-    python -m file.excel_merger
-else
-    # zepto must be run first, as it uses proxy, and would close browser when finished
-    python -m web.zepto.batcher -s -e -c
-    python -m web.blinkit.batcher -s -e
-    # swiggy must be run last, as it uses data of blinkit and zepto
-    python -m web.swiggy.batcher -s -e -c
-    python -m file.excel_merger
 fi
 
-# ./cli/run.sh -e
-# ./cli/run.sh
+if [ "$is_postprocess" = true ]; then
+    python -m file.excel_merger
+    python -m file.email
+fi
+
+# Usage:
+# ./cli/run.sh            # do nothing
+# ./cli/run.sh -s         # scrape only
+# ./cli/run.sh -e         # extract only
+# ./cli/run.sh -x         # post-process only
+# ./cli/run.sh -e -x      # extract and post-process
+# ./cli/run.sh -s -e -x   # scrape, extract and post-process
