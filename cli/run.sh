ce8eb50ccf6c50d7a4a7a035457a957a790fcc69
Hansimov <591172499@qq.com>
2025-07-27 14:50:37 +0800

    :zap: [Enhance] run.sh: add new -w arg to package and send weekly summary, and fix typo in scrape batcher

--- cli/run.sh
+++ cli/run.sh
@@ -1,6 +1,7 @@
 is_scrape=false
 is_extract=false
 is_postprocess=false
+is_weekly=false
 
 for arg in "$@"; do
     if [ "$arg" = "-s" ]; then
@@ -9,15 +10,17 @@ for arg in "$@"; do
         is_extract=true
     elif [ "$arg" = "-x" ]; then
         is_postprocess=true
+    elif [ "$arg" = "-w" ]; then
+        is_weekly=true
     fi
 done
 
 if [ "$is_scrape" = true ]; then
     # zepto must run first, as it uses proxy, and would close browser when finished
-    python -m web.zepto.scraper -s -c
-    python -m web.blinkit.scraper -s
+    python -m web.zepto.batcher -s
+    python -m web.blinkit.batcher -s
     # swiggy must run last, as it uses data of blinkit and zepto
-    python -m web.swiggy.scraper -s -c
+    python -m web.swiggy.batcher -s
 fi
 
 if [ "$is_extract" = true ]; then
@@ -27,8 +30,13 @@ if [ "$is_extract" = true ]; then
 fi
 
 if [ "$is_postprocess" = true ]; then
-    python -m file.excel_merger
+    python -m file.excel_merger -m
     python -m file.email
+fi
+
+if [ "$is_weekly" = true ]; then
+    python -m file.excel_merger -p
+    python -m file.email -c -t weekly
 fi
 
 # Usage:
@@ -38,3 +46,4 @@ fi
 # ./cli/run.sh -x         # post-process only
 # ./cli/run.sh -e -x      # extract and post-process
 # ./cli/run.sh -s -e -x   # scrape, extract and post-process
+# ./cli/run.sh -w         # weekly summary
