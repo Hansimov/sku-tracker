2570674c1f4e0b83aabfc06a5fbd4705f603b6d2
Hansimov <591172499@qq.com>
2025-08-20 03:38:47 +0800

    :zap: [Enhance] run.sh: add -d arg to adapt for batcher and excel_merger

--- cli/run.sh
+++ cli/run.sh
@@ -2,8 +2,11 @@ is_scrape=false
 is_extract=false
 is_postprocess=false
 is_weekly=false
+date_arg=""
 
+i=0
 for arg in "$@"; do
+    i=$((i + 1))
     if [ "$arg" = "-s" ]; then
         is_scrape=true
     elif [ "$arg" = "-e" ]; then
@@ -12,31 +15,37 @@ for arg in "$@"; do
         is_postprocess=true
     elif [ "$arg" = "-w" ]; then
         is_weekly=true
+    elif [ "$arg" = "-d" ]; then
+        next_i=$((i + 1))
+        eval "date_value=\${$next_i}"
+        if [ -n "$date_value" ]; then
+            date_arg="-d $date_value"
+        fi
     fi
 done
 
 # -s
 if [ "$is_scrape" = true ]; then
     # zepto must run first, as it uses proxy, and would close browser when finished
-    python -m web.zepto.batcher -s
-    python -m web.blinkit.batcher -s
-    python -m web.dmart.batcher -s
+    python -m web.zepto.batcher -s $date_arg
+    python -m web.blinkit.batcher -s $date_arg
+    python -m web.dmart.batcher -s $date_arg
     # swiggy must run last, as it uses data of blinkit and zepto
-    python -m web.swiggy.batcher -s
+    python -m web.swiggy.batcher -s $date_arg
 fi
 
 # -e
 if [ "$is_extract" = true ]; then
-    python -m web.zepto.batcher -e
-    python -m web.blinkit.batcher -e
-    python -m web.swiggy.batcher -e
-    python -m web.dmart.batcher -e
+    python -m web.zepto.batcher -e $date_arg
+    python -m web.blinkit.batcher -e $date_arg
+    python -m web.swiggy.batcher -e $date_arg
+    python -m web.dmart.batcher -e $date_arg
 fi
 
 # -x
 if [ "$is_postprocess" = true ]; then
-    python -m file.excel_merger -m -k
-    python -m file.email
+    python -m file.excel_merger -m -k $date_arg
+    python -m file.email $date_arg
 fi
 
 # -w
@@ -53,3 +62,5 @@ fi
 # ./cli/run.sh -e -x      # extract and post-process
 # ./cli/run.sh -s -e -x   # scrape, extract and post-process
 # ./cli/run.sh -w         # weekly summary
+
+# ./cli/run.sh -e -d "2025-08-19"  # extract for date
